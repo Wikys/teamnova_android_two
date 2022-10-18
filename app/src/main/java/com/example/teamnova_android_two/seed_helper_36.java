90780531e69f0d9bc6f88f1c9ef7b9692df7e8e4
ayc0812 <ayc0812@knou.ac.kr>
2022-10-19 01:45:19 +0900

    시드 36층 로직완성

--- app/src/main/java/com/example/teamnova_android_two/seed_helper_36.java
+++ app/src/main/java/com/example/teamnova_android_two/seed_helper_36.java
@@ -3,6 +3,10 @@ package com.example.teamnova_android_two;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 
 public class seed_helper_36 extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class seed_helper_36 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.seed_helper_36);
+
+        Button 초기화 = (Button) findViewById(R.id.초기화);
+        RadioGroup 단계1 = (RadioGroup) findViewById(R.id.단계1);
+        RadioGroup 단계2 = (RadioGroup) findViewById(R.id.단계2);
+        RadioGroup 단계3 = (RadioGroup) findViewById(R.id.단계3);
+        RadioGroup 단계4 = (RadioGroup) findViewById(R.id.단계4);
+        RadioGroup 단계5 = (RadioGroup) findViewById(R.id.단계5);
+        RadioGroup 단계6 = (RadioGroup) findViewById(R.id.단계6);
+        RadioGroup 단계7 = (RadioGroup) findViewById(R.id.단계7);
+        RadioGroup 단계8 = (RadioGroup) findViewById(R.id.단계8);
+
+
+        초기화.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { // 클릭시 모든 라디오버튼 초기화
+                단계1.clearCheck();
+                단계2.clearCheck();
+                단계3.clearCheck();
+                단계4.clearCheck();
+                단계5.clearCheck();
+                단계6.clearCheck();
+                단계7.clearCheck();
+                단계8.clearCheck();
+
+
+            }
+        });
     }
 }
