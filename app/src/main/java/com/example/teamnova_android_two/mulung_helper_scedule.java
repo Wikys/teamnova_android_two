196795977a78b9a782d28a7fc173a449e10df734
6층 <ayc0812@gmail.com>
2022-10-22 18:15:24 +0900

    팝업 액티비티 로직짜는중

--- app/src/main/java/com/example/teamnova_android_two/mulung_helper_scedule.java
+++ app/src/main/java/com/example/teamnova_android_two/mulung_helper_scedule.java
@@ -1,29 +1,24 @@
 package com.example.teamnova_android_two;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class mulung_helper_scedule extends Activity {
+
     private final int id = 0x8000; //버튼아이디 변수
     private Integer numButton = 0; // 버튼의 개수
     ArrayList<String> buttonNameList = new ArrayList<>(); //만든 버튼 이름들이 저장되는곳
     //버튼 추가되는 레이아웃
+    LinearLayout 저장목록;
+    int 버튼id;
 
 
     @Override
@@ -33,6 +28,7 @@ public class mulung_helper_scedule extends Activity {
         setContentView(R.layout.mulung_helper_scedule);
 
         Button save = (Button) findViewById(R.id.저장버튼);
+
 
         save.setOnClickListener(new View.OnClickListener() { //버튼생성 이벤트
             @Override
@@ -53,10 +49,10 @@ public class mulung_helper_scedule extends Activity {
             }
         });
         Button delete = (Button) findViewById(R.id.삭제버튼);
-
         delete.setOnClickListener(new View.OnClickListener() { //저장한버튼 삭제
             @Override
             public void onClick(View view) {
+                removeList();
 
 
             }
@@ -64,20 +60,27 @@ public class mulung_helper_scedule extends Activity {
     }
 
     private void newSaveList() { // 저장목록에 버튼추가하는 메소드
-        LinearLayout 저장목록 = (LinearLayout) findViewById(R.id.저장목록); // 버튼이 생성될 레이아웃
+        LinearLayout 저장목록 = (LinearLayout) findViewById(R.id.저장목록);
+        // 버튼이 생성될 레이아웃
         EditText title = (EditText) findViewById(R.id.제목); //사용자가 정한 제목
         numButton++; // 버튼아이디 뒤에 붙일 숫자
         Button savelist = new Button(this); // 버튼생성
         savelist.setText(title.getText()); //제목에 입력한 텍스트를 버튼이름으로 지정
         savelist.setId(id+numButton); //제목에 입력한 텍스트를 아이디로 지정
+        savelist.setOnClickListener(new View.OnClickListener() { // 버튼클릭하면 작동할코드
+            public void onClick(View v) { // 아이디가 v에 저장됨
+                //버튼의 아이디를 리무브리스트에 전달해줄 방법 생각...
+                버튼id = v.getId();
+            }
+        });
         저장목록.addView(savelist, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
         //시간이나 제목이 동일하면 호출불가하게 설정
         //텍스트내용이랑 시간정보도 보내줘야함
     }
     private void removeList(){
-        if( numButton <= 0){ // 만약 View가 0개 이하일 때
-            return;
-        }// 클릭한 뷰의 아이디를 받아와서 삭제해줘야함
+        // 클릭한 뷰의 아이디를 받아와서 삭제해줘야함
+        Button 동적버튼 = findViewById(버튼id); // 생성했던 View의 ID 가져오기
+        저장목록.removeView(동적버튼);
 
     }
 
@@ -101,4 +104,11 @@ public class mulung_helper_scedule extends Activity {
     protected void onStop() { // 온스탑이될때 저장목록에 있는 시간,메모정보를 메인에 쏴줘야함
         super.onStop();
     }
+
+
+//    public void onClick(View v) { //선택한뷰 아이디값 가져와서 삭제버튼에 보내주고
+//        //삭제버튼에서 받아서 삭제
+//        동적버튼 = findViewById(v.getId()); //맴버변수 값을 아이디로 변경
+//
+//    }
 }
