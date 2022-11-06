570f21b4a9451d8e91cc69d51b2d336dab44c807
Wikys <ayc0812@gmail.com>
2022-11-06 18:09:57 +0900

    사인업액티비티 마무리하면됨

--- app/src/main/java/com/example/teamnova_android_two/MainActivity.java
+++ app/src/main/java/com/example/teamnova_android_two/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.teamnova_android_two;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.lifecycle.Lifecycle;
 
+import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -24,6 +30,18 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private BroadcastReceiver alarm;
+
+    ActivityResultLauncher<Intent> receive_Id_Result = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        //겟엑스트라 입력
+                    }
+                }
+            });
+
 
 
     @Override
@@ -44,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "로그인에 성공하였습니다", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(MainActivity.this, Main.class);
                     startActivity(intent);
+
                 } else {
                     Toast.makeText(MainActivity.this, "아이디와 비밀번호를 확인 해주세요", Toast.LENGTH_SHORT).show();
                 }
@@ -54,12 +73,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, sign_up.class);
-                ActivityResultLauncher
-
+                receive_Id_Result.launch(intent);
             }
         });
 
     }
+
+
 }
 //데이터 단방향전송할까하다가
 //그렇게되면 스타트액티비티로 다시 돌아와야하는데 생명주기꼬임
